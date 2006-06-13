3cc36d4fb472e9974a0d24ae4055ad33392c0234
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-06-13 19:23:31 +0000

    new query to only return necessary components for table of contents

--- html/toc.php
+++ html/toc.php
@@ -15,11 +15,46 @@ $xsl_file 	= "stylesheets/marblfa.xsl";
 $xsl_params = array('mode' => 'toc');
 
 // query for all volumes 
-$query = "
+/*$query = "
 	for \$a in input()/ead 
 	where \$a/@id='$id' 
 	return \$a
 ";
+
+*/
+
+$query = "for \$a in input()/ead 
+let \$ad := \$a/archdesc
+where \$a/@id='$id' 
+return <ead>
+{\$a/@id}
+<eadheader>
+ <filedesc>
+  <titlestmt>
+   {\$a/eadheader/filedesc/titlestmt/titleproper}
+  </titlestmt>
+ </filedesc>
+</eadheader>
+<archdesc>
+ <did>
+ {\$ad/did/unittitle}
+ </did>
+ <dsc>
+ {\$ad/dsc/head}
+ {for \$c in \$ad/dsc/c01 
+  return <c01> 
+  {\$c/@id} {\$c/@level}
+  <did>
+   {\$c/did/unittitle}
+   {\$c/did/physdesc}
+  </did>
+{-- adding c02 so c01 will display in xslt (kind of a hack) --}
+  <c02/>
+  </c01>}
+ </dsc>
+</archdesc>
+</ead>";
+
 
 $rval = $tamino->xquery(trim($query));
 $tamino->xslTransform($xsl_file, $xsl_params);
