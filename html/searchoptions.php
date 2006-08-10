28da0f8bffaed307d018587d0ce8d374530abf47
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-08-10 14:37:10 +0000

    searchtip wording changes requested by Susan

--- html/searchoptions.php
+++ html/searchoptions.php
@@ -3,17 +3,17 @@
 <table name="searchtable">
 <tr><td>
 
-<h2>Search</h2>
+<h2 class="alphaText">Search Collections</h2>
 
 <form name="fa_query" action="search" method="post">
 <table class="searchform" border="0">
 <tr><th>Keyword</th><td class="input"><input type="text" size="40" name="keyword" value="<?php print $kw ?>"></td></tr>
-<tr><th></th><td><span class="support_text">Searches entire text of finding aid</span></td></tr>
+<tr><th></th><td class="info"><span class="support_text">Searches entire text of finding aid</span></td></tr>
 
 
 <tr><th>Creator</th><td class="input"><input type="text" size="40" name="author" value="<?php print $author ?>"></td></tr>
-<tr><th></th><td><span class="support_text">Searches only for person, family, or organization that created or accumulated the collection</span></td></tr>
-<tr><th></th><td><span class="support_text">[e.g.<b>Heaney, Seamus</b> or <b>Georgia Woman's Christian Temperance Union</b></span></td></tr>
+<tr><th></th><td class="info"><span class="support_text">Searches only for person, family, or organization that created or accumulated the collection</span>
+<span class="support_text">[e.g., <b>Heaney, Seamus</b> or <b>Georgia Woman's Christian Temperance Union</b></span></td></tr>
 
 
 <tr><td></td><td><input type="submit" value="Search"> <input type="reset" value="Reset"></td></tr>
@@ -24,11 +24,11 @@
 
 <td class="searchtips" valign="top">
 <ul class ="searchtips"><b>Search tips:</b>
-<li>You can words in more than one search box.</li>
-<li>Asterisks may be used to do a truncated search.<br>
-For example, enter <b>resign*</b> to match <b>resign</b>, <b>resigned</b>, and <b>resignation</b>. </li>
+<li>You can enter words in more than one search box.</li>
+<li>Asterisks may be used to do a truncated search. 
+[e.g., enter <b>resign*</b> to match <b>resign</b>, <b>resigned</b>, and <b>resignation</b>.] </li>
 <li>Capitalization is ignored.</li>
-<li>Enclose phrases in  quotation marks(")</li>
+<li>Search for exact phrases using quotation marks [e.g., <b>"harlem renaissance"</b>]
 </ul>
 </td>
 
