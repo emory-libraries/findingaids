acf8948494228c27bb801bf695fca7205646187e
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-08-15 21:24:18 +0000

    edited search tips text as requested by Susan

--- html/searchoptions.php
+++ html/searchoptions.php
@@ -8,12 +8,11 @@
 <form name="fa_query" action="search" method="post">
 <table class="searchform" border="0">
 <tr><th>Keyword</th><td class="input"><input type="text" size="40" name="keyword" value="<?php print $kw ?>"></td></tr>
-<tr><th></th><td class="info"><span class="support_text">Searches entire text of finding aid</span></td></tr>
+<tr><th></th><td class="info">Searches entire text of finding aid</td></tr>
 
 
 <tr><th>Creator</th><td class="input"><input type="text" size="40" name="author" value="<?php print $author ?>"></td></tr>
-<tr><th></th><td class="info"><span class="support_text">Searches only for person, family, or organization that created or accumulated the collection</span>
-<span class="support_text">[e.g., <b>Heaney, Seamus</b> or <b>Georgia Woman's Christian Temperance Union</b></span></td></tr>
+<tr><th></th><td class="info">Searches only for person, family, or organization that created or accumulated the collection [e.g., <b>Heaney, Seamus</b> or <b>Georgia Woman's Christian Temperance Union</b>]</td></tr>
 
 
 <tr><td></td><td><input type="submit" value="Search"> <input type="reset" value="Reset"></td></tr>
