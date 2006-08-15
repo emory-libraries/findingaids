aee2fa1334da8d44679c3d034ba5e961d3f32e71
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-08-15 21:31:13 +0000

    file no longer in use

--- html/findingAid.php
+++
@@ -1,7 +0,0 @@
-<?
-
-require_once("alphaBox.php");
-
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/marblfa.css\">\n";
-alphaBox("Jump within results alphabetically:", "browse-coll");
-?>
