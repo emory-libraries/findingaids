92a7f6c7151efd4b58ef3f843abea199171d273d
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2007-02-05 22:55:47 +0000

    slight text & formatting changes for front page

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ include_once("common_functions.php");
 html_head("Irish Literary Collections");
 include("template-header.inc");
 
+print "<div id='index'>\n";
+
 // FIXME: edit alt/title text for these images (get descriptions from Susan)
 $img[0] = array("images/home-1.jpg", "sample book covers of Irish holdings");
 $img[1] = array("images/home-2.jpg", "sample manuscripts from Irish holdings");
@@ -15,6 +17,11 @@ $img[2] = array("images/home-3.jpg", "sample broadsides and theater programs fro
 $n = rand(0, (count($img) - 1));
 print "<img id='frontpage' src='" . $img[$n][0] . "' alt='" . $img[$n][1] . "' title='" . $img[$n][1] . "'/>";
 
+
+print "<p>The Irish Literary Collections Portal provides access to the finding aids of
+over 100 of North America's Irish literary manuscript collections.</p>\n";
+
+print "</div>\n";
 
 include("template-footer.inc");
 
