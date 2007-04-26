400655012f957d34ead95b43504d22c5fbb5f8db
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2007-04-26 20:44:53 +0000

    only override error reporting level in development

--- config.php
+++ config.php
@@ -2,9 +2,12 @@
 
 /* Configuration settings for entire site */
 
-// set level of php error reporting -- turn off warnings when in production
-error_reporting(E_ALL ^ E_NOTICE);
+$in_production = false;
 
+// set level of php error reporting (warnings should be turned off in production)
+if (! $in_production) {
+  error_reporting(E_ALL ^ E_NOTICE);
+}
 
 /* tamino settings  */
 $tamino_server = "vip.library.emory.edu";
