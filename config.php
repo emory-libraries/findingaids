3301cab45cc8dbab2b2c5ebd2b1cf5c2a652a5bc
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2007-04-26 20:48:24 +0000

    only override error reporting level in development; easier to switch production/dev settings

--- config.php
+++ config.php
@@ -2,18 +2,29 @@
 
 /* Configuration settings for entire site */
 
-// set level of php error reporting -- turn off warnings when in production
-error_reporting(E_ALL ^ E_NOTICE);
+$in_production = false;
+
+// set level of php error reporting (warnings should be turned off in production)
+if (! $in_production) {
+  error_reporting(E_ALL ^ E_NOTICE);
+}
+
+/* tamino settings  */
+$tamino_server = "vip.library.emory.edu";
+$tamino_db = "EAD_TEST";
+$tamino_coll = "MARBLFindingAids";
+
 
 // settings for generating pdfs
 $tmpdir = "/tmp/findingaids/";
 $fop = "http://localhost:8080/fop/fop?fo=";
 
 /* exist settings  */
-// test
-$server = "wilson.library.emory.edu";
-//production
-//$server = "bohr.library.emory.edu";
+if ($in_production) {
+  $server = "bohr.library.emory.edu";		//production
+} else {
+  $server = "wilson.library.emory.edu";  	// test
+}
 $port = "8080";
 $db = "FindingAids";
 
