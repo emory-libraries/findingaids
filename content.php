12da357dc3f632b3d9ed9ad5296d269415088f24
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2007-05-09 22:04:04 +0000

    extracted logic into xquery functions; started adding exact phrase

--- content.php
+++ content.php
@@ -4,292 +4,111 @@ include_once("lib/xmlDbConnection.class.php");
 include_once("common_functions.php");
 include("marblcrumb.class.php");
 
+$connectionArray{"debug"} = false;
+
 
 $id = $_GET["id"];
 if (isset($_GET["el"])) 
   $element = $_GET["el"];
 $kw = $_GET["keyword"];
 
+$acceptable_els = array("c01", "c02", "c03", "did", "ead", "index");
+
 if (empty($element)) {
   $element = "ead";		// default element to retrieve
- } else if (!($element == "c01" || $element ==  "c02" || $element == "c03"
-	      || $element == "did" || $element == "ead" || $element == "index")) {
+  $mode = "summary";
+ } else if (! in_array($element, $acceptable_els)) {
   // make sure that the element is something we expect
-  print "DEBUG: element $el not expected, quitting.\n";
-  // FIXME: add a better error message here
+  print "Error: element <b>$element</b> not expected, quitting.\n";
+  // FIXME: add a better error message here?
   exit();
  }
 
 
-
-
-$connectionArray{"debug"} = false;
-
  
+//$kwarray = processterms($kw);
+// keyword is being passed in as a | delimited string
+$kwarray = array();
+$phrases = array();
+$keywords = "";
+foreach (explode('|', $kw) as $k) {
+  if (preg_match("/_/", $k)) {
+    $k = preg_replace("/_/", " ", $k);
+    array_push($phrases, addslashes($k));
+  } else {
+    array_push($kwarray, $k);
+    $keywords .= " " . addslashes($k);
+  }
+}
 
-//echo "<pre>";print_r($_REQUEST);echo "</pre>";
 
-
-	// determine xpath for query according to element
-	switch ($element)
-	{
-		case 'c01':
-		  $path = "/archdesc/dsc/c01";
-		  $wrapOutput = true;
-		  break;
-		
-		case 'c02':
-		  $path = "/archdesc/dsc/c01/c02";
-		  $wrapOutput = true;
-		break;
-
-		case 'c03':
-		  $path = "/archdesc/dsc/c01/c02/c03";
-		  $wrapOutput = true;
-		break;
-
-		case 'did':
-		  $path = "/archdesc/did";
-		  $wrapOutput = true;
-		break;
-
-		case 'index':
-		  $path = "/archdesc/index";
-		  $wrapOutput = true;
-		break;
-
-		case 'ead':
-		default:
-		  // query for all volumes 
-		  $path = "";
-		  $mode = 'summary';
-		  
-		  $wrapOutput = false;
-	}
-
-	//$kwarray = processterms($kw);
-	// keyword is being passed in as a | delimited string
-	$kwarray = array();
-	$phrases = array();
-	$keywords = "";
-	foreach (explode('|', $kw) as $k) {
-	  if (preg_match("/_/", $k)) {
-	    $k = preg_replace("/_/", " ", $k);
-	    array_push($phrases, addslashes($k));
-	  } else {
-	    array_push($kwarray, $k);
-	    $keywords .= " " . addslashes($k);
-	  }
-	  }
-	  //	$kwarray = explode('|', $kw);
-	  //	$kw = preg_replace("/\|/", " ", $kw);  // multiple white spaces become one space
+$xmldb = new xmlDbConnection($connectionArray);
 	
-	//echo "<pre>"; print_r($kwarray); echo "</pre>";
+// using filters to restrict element path in 'for' statement
+$filter = array();
+// only add a keyword filter if there are keywords defined
+if ($keywords != ' ' and $keywords != '') array_push($filter, ". |= '$keywords'");
+foreach ($phrases as $p)
+// eXist's near function finds the terms in this order, by default 1 word away
+// (it does, however count & match every occurrence of the terms in the phrase)
+  array_push($filter, "near(., '$p')");
 
-	//	$element = (isset($_REQUEST['element'])) ? $_REQUEST['element'] : null;
-	//$mode = ($element == 'did' || $element == 'c01') ? 'c-level-index' : 'summary';
-	//$mode = 'table';
-		
-	$tamino = new xmlDbConnection($connectionArray);
-	
-	// using filters to restrict element path in 'for' statement
-	$filter = array();
-	// only add a keyword filter if there are keywords defined
-	if ($keywords != ' ' and $keywords != '') array_push($filter, ". |= '$keywords'");
-	foreach ($phrases as $p)
-	  // eXist's near function finds the terms in this order, by default 1 word away
-	  // (it does, however count & match every occurrence of the terms in the phrase)
-	  array_push($filter, "near(., '$p')");
+// using an "or" filter to highlight any of the search terms that occur in any section of the document
+if (count($filter)) {	// keyword/phrase mode (at least one filter)
+  $orfilter = "[" . implode(" or ", $filter) . "]";
+ } else {
+  $orfilter = "";
+ }
 
-	// using an "or" filter to highlight any of the search terms that occur in any section of the document
-	if (count($filter)) {	// keyword/phrase mode (at least one filter)
-	  $orfilter = "[" . implode(" or ", $filter) . "]";
-	} else {
-	  $orfilter = "";
-	}
+// pull out exact phrase enclosed in quotation marks
+preg_match_all("/\"([^\"]+)\"/", stripslashes($kw), $phrases);
+$phrase = $phrases[1][0];
 
-	// note: the filter is at the top-level, since the item we are returning (c01, etc.)
-	// may or may not include the search terms, but should be returned either way
-	$toc_query = "
-		let \$a := /ead${orfilter}${path}[@id = '$id']
-		let \$doc := root(\$a)/ead
-		let \$ad := \$doc/archdesc 
-		return 
-			<toc>
-				<ead>{\$doc/@id}
-					<name>
-						{string(\$doc/archdesc/did/origination/persname)}
-						{string(\$doc/archdesc/did/origination/corpname)}
-						{string(\$doc/archdesc/did/origination/famname)}
-					</name>
-					<eadheader>
-						<filedesc><titlestmt>{\$doc/eadheader/filedesc/titlestmt/titleproper}</titlestmt></filedesc>
-					</eadheader>
-					<archdesc>
-						<did>{\$ad/did/unittitle}"; 
-	if ($kw != '') {
-	  $toc_query .= " <hits> { let \$didm := \$ad/did$orfilter
-			return text:match-count(\$didm) }</hits>";
-	}
-
-	$toc_query .= "
-						</did>";
-	// hit counts for top-level table of contents items;
-	// sum up multiple counts where the count needs to include multiple nodes
-	if ($kw != '') {
-	  $toc_query .= "<collectiondescription>
-			    <hits> { sum(for \$cdm in (\$ad//bioghist,\$ad//scopecontent)$orfilter
-			    return text:match-count(\$cdm)) }</hits>
-			 </collectiondescription>
-			 <controlaccess>
-			    <hits> { sum(for \$cam in \$ad//controlaccess$orfilter
-			    return text:match-count(\$cam)) }</hits>
-			 </controlaccess>
-		";
-	}
-	$toc_query .= "<index>{\$ad/index/@id}{\$ad/index/head} ";
-	if ($kw != '') {
-	  $toc_query .= "<hits> { text:match-count(\$ad/index$orfilter) }</hits>";
-	}
-	$toc_query .= "	</index>  
-
-						<dsc>
-							{\$ad/dsc/head}";
-	if ($kw != '') {
-	  $toc_query .= "<hits>{let \$dscm := \$ad/dsc$orfilter return text:match-count(\$dscm) }</hits>";
-	}
-	$toc_query .= "
-							{for \$c in \$ad/dsc/c01[@level='series']
-							 let \$cmatch := \$c$orfilter
-							 return  
-							 <c01> 
-								{\$c/@id} {\$c/@level}
-								{if (exists(\$c/c02)) then <c02/> else ()}\n";
-	if ($kw != '') { 
-	  $toc_query .= "<hits>{text:match-count(\$cmatch)}</hits>\n";
-	} 
-	$toc_query .= "
-								<did>
-									{\$c/did/unitid}
-									{\$c/did/unittitle}
-									{\$c/did/physdesc}
-								</did>
-							</c01>}
-						</dsc>
-					</archdesc>
-				</ead>
-			</toc>
-	";
+$keywords = preg_replace("/\s*\"[^\"]+\"\s*/", "", $kw);
 
 
-	// addition to query for highlighting (when there are search terms)
-	$hquery = "";
-	$rval = "\$a";
-        $hitcount = "";
-	if ($kw != '') { 
-	  $hitcount .= "{let \$m := \$c01$orfilter return <hits>{text:match-count(\$m)}</hits>}\n";
-	}
 
-	if ($element == "ead") {		// return only necessary sections, not the entire document
-		  $rval = "<ead>{\$a/@id}
-		  {\$a/eadheader}
-		  {\$a/frontmatter}
-		 <archdesc>
-		  {for \$el in \$a/archdesc/*[not(self::dsc)]
-		    let $rval := ( if (exists(\$el$orfilter)) then
-			\$el$orfilter
-		     else \$el)
-  		   return $rval }
-		  {for \$d in \$a/archdesc/dsc
-		   return <dsc> {\$d/@*}
-		   {\$d/head}
-		   {for \$i in \$d/c01[@level!='series']
-		    return if (exists(\$i$orfilter))
-			  then \$i$orfilter
-			else \$i }
-		   {for \$c01 in \$d/c01[c02 or @level='series']
-		    return <c01>
-		      {\$c01/@*}
-		      {\$c01/did}
-		       $hitcount
-		      {for \$c02 in \$c01/c02[@level='subseries']
-			return <c02>
-				{\$c02/@*}
-				{\$c02/did}
-				{let \$m2 := \$c02$orfilter
-				  return <hits>{text:match-count(\$m2)}</hits>}
-			      {for \$c03 in \$c02/c03[c04]
-				return <c03>{\$c03/@*}
-					{\$c03/did}
-					{let \$m3 := \$c03$orfilter
-					  return <hits>{text:match-count(\$m3)}</hits>}
-				       </c03>}
-			      </c02> }
-		     </c01> }
-		     </dsc>}
-		 </archdesc></ead> ";
-	}
+//$xquery = $eadxq . 'let $a := ' . "/ead${orfilter}${path}[@id = '$id'] " . '
+$xquery = $eadxq . "\n" .  'let $a := ' . "//${element}[@id = '$id'] " . '
+let $ead := root($a)/ead  
+return <results> 
+   {eadxq:toc($ead, "' . $keywords . '", "' . $phrase . '") } 
+   {eadxq:content($a, "' . $keywords . '", "' . $phrase . '")} 
+</results>';
+// note: doesn't yet handle highlighting, hit counts completely 
+  
 
 
-	// all elements other than ead must be wrapped in an ead node
-	if ($wrapOutput) {
-	  // return parent c01 id so we can highlight current section in table of contents
-		$rval = "<ead>{ $rval } <parent id='{\$a/ancestor::c01/@id}'/></ead>";
-		       
-	}
+$xsl_file = "stylesheets/marblfa.xsl";
 
-
-	/*	$query = "
-		for \$doc in input()/ead
-		for \$a in \$doc$path
-		$hquery
-		where \$a/@id='$id' 
-		return $rval";
-	*/
-
-	// section may or may not contain keywords; return with highlighting if it does
-	$query = "let \$i := /ead${path}[@id = '$id'] ";
-	if ($kw != '')
-	  $query .= "let \$im := \$i$orfilter
-		  let \$a := (if (exists(\$im)) then
-		      \$im
-		  else  \$i)
-		  return $rval";
-	else
-	  $query .= "let \$a := \$i
-		     return $rval";
-	//		  return if (exist$rval";
-
-	$xsl_file 	= "stylesheets/marblfa.xsl";
-	// xslt passes on keywords to subsections of document via url
-	$term_list = urlencode(implode("|", array_merge($kwarray, $phrases)));
+// xslt passes on keywords to subsections of document via url
+$term_list = urlencode(implode("|", array_merge($kwarray, $phrases)));
 if ($kw != '') 
   $xsl_params = array("url_suffix"  => "&keyword=$kw");
 	
-	
-	//$xquery = "$declare <results>{ $toc_query } { $query }</results>";
-	$xquery = "<results>{ $toc_query } { $query }</results>";
-	//echo "$xquery<hr>";
-	$rval = $tamino->xquery(trim($xquery));
-	$tamino->xslTransform($xsl_file, $xsl_params);
-	
+//$xquery = "$declare <results>{ $toc_query } { $query }</results>";
+//$xquery = "$declare <results>{ $toc_query } { $query }</results>";
+//echo "$xquery<hr>";
+$rval = $xmldb->xquery(trim($xquery));
+$xmldb->xslTransform($xsl_file, $xsl_params);
+
 
 // get unittitle, but add spaces before any unitdates 
-$docname = $tamino->findNode('archdesc/did/unittitle/text()');
-if ($ud = $tamino->findNode('archdesc/did/unittitle/unitdate[1]'))
+$docname = $xmldb->findNode('archdesc/did/unittitle/text()');
+if ($ud = $xmldb->findNode('archdesc/did/unittitle/unitdate[1]'))
   $docname .= " " . $ud;
-if ($ud = $tamino->findNode('archdesc/did/unittitle/unitdate[2]'))
+if ($ud = $xmldb->findNode('archdesc/did/unittitle/unitdate[2]'))
   $docname .= " " . $ud;
-//$docname = $tamino->findNode('archdesc/did/unittitle/text()'); 
+//$docname = $xmldb->findNode('archdesc/did/unittitle/text()'); 
 
 	$pagename = $docname;
 //	$crumbs[2] = array ('anchor' => $docname);
 	$htmltitle = "$docname";
 if ($element == 'index') {
-  $pagename = $tamino->findNode("results/ead/$element/head");
+  $pagename = $xmldb->findNode("results/ead/$element/head");
   $htmltitle .= " [$pagename]";
 } else if ($element != "ead") {
-  $pagename = $tamino->findNode("results/ead/$element//unittitle");
+  $pagename = $xmldb->findNode("results/ead/$element//unittitle");
   $htmltitle .= " [$pagename]";
  }
 
@@ -310,7 +129,7 @@ include("template-header.inc");
 print $crumbs;
 
 print '<div class="content">';
-$tamino->printResult();
+$xmldb->printResult();
 print '</div>';
 
 include("template-footer.inc");
