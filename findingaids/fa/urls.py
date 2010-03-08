750bca9dde6fdf15458bc3bfb9eb6af4549ba29c
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2010-03-08 23:10:38 +0000

    tweak document id url regex

--- findingaids/fa/urls.py
+++ findingaids/fa/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('findingaids.fa.views',
                        (r'^titles/?$', 'browse_titles'),
                        (r'^titles/(?P<letter>[A-Z])$', 'titles_by_letter'),
-                       (r'^documents/(?P<id>[a-z0-9-.]+)$', 'view_fa'),
-                       (r'^documents/(?P<id>[a-z0-9-.]+)/(?P<series_id>[a-zA-Z0-9._]+)$', 'view_series'),
-                       (r'^documents/(?P<id>[a-z0-9-.]+)/(?P<series_id>[a-zA-Z0-9._]+)/(?P<subseries_id>[a-zA-Z0-9._]+)$',
+                       (r'^documents/(?P<id>[A-Za-z0-9-.]+)$', 'view_fa'),
+                       (r'^documents/(?P<id>[A-Za-z0-9-.]+)/(?P<series_id>[a-zA-Z0-9._]+)$', 'view_series'),
+                       (r'^documents/(?P<id>[A-Za-z0-9-.]+)/(?P<series_id>[a-zA-Z0-9._]+)/(?P<subseries_id>[a-zA-Z0-9._]+)$',
                         	'view_subseries'),
 )
  
