90fc3f4dced2746c9c9d58c9dd3f9c48b7f35365
Ben Ranker <branker@emory.edu>
2010-04-12 19:42:20 +0000

    docfairy visit:  * add findingaids.__version__ (and __version_info__) and pull them for    findingaids docs  * change copy-and-paste eulcore references to findingaids  * clean up doc/conf.py  * remove a few ugly underscores in filenames  * remove _build (cum build) from svn  * elimintate a doc warning by rewording /README a bit

--- doc/doc_djangosettings.py
+++ doc/doc_djangosettings.py
@@ -1,8 +1,7 @@
 # This dummy django settings file is used by sphinx while loading
-# eulcore.django.* to examine it for autodoc generation.
+# findingaids.* to examine it for autodoc generation.
 
 # NOTE: something being included needs eXist to be set to something valid
-
 EXISTDB_SERVER_URL      = 'http://edc_user:emory@kamina.library.emory.edu:8080/exist/xmlrpc'
 EXISTDB_ROOT_COLLECTION = "/FindingAids/emory"
 EXISTDB_TEST_COLLECTION = "/fa-test"
