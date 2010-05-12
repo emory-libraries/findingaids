89c3a1278ce070080f2a7f8352a394909cdc1be8
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2010-05-12 22:09:08 +0000

    sanity check function & tests #38

--- findingaids/admin/tests.py
+++ findingaids/admin/tests.py
@@ -1,10 +1,12 @@
+import os
+import tempfile
+from time import sleep
 from django.test import Client
 from django.conf import settings
 from eulcore.django.test import TestCase
+from eulcore.django.existdb.db import ExistDB
 from findingaids.admin.views import _get_recent_xml_files
-import tempfile
-from time import sleep
-import os
+from findingaids.admin.utils import check_ead
 
 class AdminViewsTest(TestCase):
     # create temporary directory with files for testing
@@ -72,4 +74,43 @@ class AdminViewsTest(TestCase):
         #print response
         #print response.status
         #print self.client.session
-        
+
+class UtilsTest(TestCase):
+    
+    def test_check_ead(self):
+        # check valid EAD - no errors  -- good fixture, should pass all tests
+        dbpath = settings.EXISTDB_TEST_COLLECTION + '/hartsfield588.xml'
+        valid_eadfile = os.path.join(settings.BASE_DIR, 'admin', 'fixtures', 'hartsfield558.xml')
+        errors = check_ead(valid_eadfile, dbpath)
+        self.assertEqual(0, len(errors))
+
+        # should cause several errors - not DTD valid, eadid, series/subseries ids missing, index id missing
+        errors = check_ead(os.path.join(settings.BASE_DIR, 'admin', 'fixtures', 'hartsfield558_invalid.xml'),
+                dbpath)
+        self.assertNotEqual(0, len(errors))
+        self.assert_("Attribute 'invalid' not declared" in errors[0])   # validation error
+        self.assert_("eadid 'hartsfield558.xml' does not match expected value" in errors[1])
+        self.assert_("series c01 id attribute is not set for Series 1" in errors[2])
+        self.assert_("subseries c02 id attribute is not set for Subseries 6.1" in errors[3])
+        self.assert_("index id attribute is not set for Index of Selected Correspondents" in errors[4])
+
+        # eadid uniqueness check in db
+        db = ExistDB()
+        db.load(open(valid_eadfile), dbpath, True)
+        errors = check_ead(valid_eadfile, dbpath)
+        # same eadid, but present in the file that will be updated - no errors
+        self.assertEqual(0, len(errors))
+
+        # upload same file to a different path - non-unique eadid error
+        db.load(open(valid_eadfile), settings.EXISTDB_TEST_COLLECTION + '/harstfield_other.xml', True)
+        errors = check_ead(valid_eadfile, dbpath)
+        self.assertEqual(1, len(errors))
+        self.assert_("Database already contains 2 instances of eadid" in errors[0])
+
+        # remove version with correct path to test single conflicting eadid
+        db.removeDocument(dbpath)
+        errors = check_ead(valid_eadfile, dbpath)
+        self.assertEqual(1, len(errors))
+        self.assert_("Database contains eadid 'hartsfield558' in a different document" in errors[0])
+        db.removeDocument(settings.EXISTDB_TEST_COLLECTION + '/harstfield_other.xml')
+
---
+++ findingaids/admin/utils.py
@@ -0,0 +1,68 @@
+import os
+from Ft.Xml import ReaderException
+from eulcore.xmlmap.core import load_xmlobject_from_file
+from findingaids.fa.models import FindingAid
+
+def check_ead(filename, dbpath):
+    """
+    Sanity check an EAD file before it is loaded to the configured database.
+
+    Checks the following:
+     - DTD valid (file must include doctype declaration)
+     - eadid matches expected pattern (filename without .xml)
+     - check that eadid is unique within the database (only present once, in the file that will be updated)
+     - series and index ids are present
+     
+
+    :param filename: full path to the EAD file to be checked
+    :param dbpath: full path within eXist where the document will be saved
+    """
+    errors = []
+    try:
+        ead = load_xmlobject_from_file(filename, FindingAid, validate=True)
+    except ReaderException, e:
+        errors.append(e)
+        # if not dtd-valid, load without validation to do additional checking
+        ead = load_xmlobject_from_file(filename, FindingAid, validate=False)
+    
+    # eadid is expected to match filename without .xml extension
+    expected_eadid = os.path.basename(filename).replace('.xml', '')
+    if ead.eadid != expected_eadid :
+        errors.append("eadid '%s' does not match expected value of '%s'" % (ead.eadid, expected_eadid))
+    else:   # if eadid is acceptable, check for uniqueness in configured database
+        fa = FindingAid.objects.filter(eadid=ead.eadid).only("document_name", "collection_name")
+        if fa.count() > 1:
+            errors.append("Database already contains %s instances of eadid '%s'! (%s)"
+                    % (fa.count(), ead.eadid, ", ".join([f.document_name for f in fa])))
+        elif fa.count() == 1:
+            # some inconsistency in when /db is included on exist collection names
+            path = fa[0].collection_name.replace('/db', '') + "/" + fa[0].document_name
+            if path != dbpath:
+                errors.append("Database contains eadid '%s' in a different document (%s)"
+                    % (ead.eadid, fa[0].document_name))            
+
+    # check that series ids are set
+    if ead.dsc.hasSeries:
+        for series in ead.dsc.c:
+            errors.extend(_check_series_ids(series))
+
+    # check that any index ids are set
+    for index in ead.archdesc.index:
+        if not index.id:
+            errors.append("%(node)s id attribute is not set for %(label)s"
+                % { 'node' : index.dom_node.nodeName, 'label' : index.head })
+    return errors   
+    
+
+
+def _check_series_ids(series):
+    # recursive function to check that series and subseries ids are present
+    errors = []
+    if not series.id:
+        errors.append("%(level)s %(node)s id attribute is not set for %(label)s"
+                % { 'node' : series.dom_node.nodeName, 'level' : series.level, 'label' : series.display_label() })
+    if series.hasSubseries():
+        for c in series.c:
+            errors.extend(_check_series_ids(c))
+    return errors
+    
