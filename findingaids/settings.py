f9e9255bcc55e8d55f86a8c9cdf2ef316a920dac
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2010-05-13 18:09:41 +0000

    add django 1.2 csrf stuff for login

--- findingaids/settings.py
+++ findingaids/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 )
 
 ROOT_URLCONF = 'findingaids.urls'
