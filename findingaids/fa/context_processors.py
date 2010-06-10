c79ba136739c8c0890b084e3eee90e9f5c1d7541
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2010-06-10 20:05:16 +0000

    rename keyword search form so it won't conflict with other forms

--- findingaids/fa/context_processors.py
+++ findingaids/fa/context_processors.py
@@ -3,7 +3,7 @@ from findingaids.fa.forms import KeywordSearchForm
 
 def searchform(request):
     "Template context processor: add the simple keyword search form to context"
-    return {'form': KeywordSearchForm()}
+    return {'kwsearch_form': KeywordSearchForm()}
 
 def version(request):
     "Template context processor: add the findingaids software version to context."
