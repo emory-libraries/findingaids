e4a83b16a703361e648169721b513e599d622737
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2010-07-14 17:39:35 +0000

    remove unused import

--- findingaids/fa/utils.py
+++ findingaids/fa/utils.py
@@ -9,7 +9,7 @@ from django.conf import settings
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.template import Context
 from django.template.loader import get_template
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import get_object_or_404
 
 from django.template import RequestContext
 
