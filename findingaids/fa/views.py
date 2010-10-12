f4e054d5aa8fe1899bbaf29299f546bfd7ec109b
Alex Thomas <athom09@emory.edu>
2010-10-12 17:07:47 +0000

    #262 - modified title_by_letter view and template to display page range lablels in the title bar

--- findingaids/fa/views.py
+++ findingaids/fa/views.py
@@ -70,6 +70,7 @@ def titles_by_letter(request, letter):
          'letters': title_letters(),
          'current_letter': letter,
          'show_pages' : show_pages,
+         'title_range' : show_pages[fa_subset.number]
     }
     if page_labels:     # if there is content and page labels to show, add to context
          # other page labels handled by show_pages, but first & last are special
