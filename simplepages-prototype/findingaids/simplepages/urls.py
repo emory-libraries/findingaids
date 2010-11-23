459d296d1ac3a4e32db71e949d9118e6422ba176
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2010-11-23 16:58:23 +0000

    moving simplepages-prototype branch to correct location

--- simplepages-prototype/findingaids/simplepages/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('findingaids.simplepages.views',
-    url(r'^(?P<url>.*)$', 'simplepage', name="viewpage"),
-)
--- simplepages-prototype/findingaids/simplepages/views.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.flatpages.views import flatpage
-
-def simplepage(request, url):
-    # Just a wrapper for the normal flatpage view.
-    return flatpage(request, url)
