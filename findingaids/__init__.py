6691902980640c83e09d50fd9ded09e13ca047ef
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-04-01 17:32:31 +0000

    updating version - prep for tagging bugfix release 1.0.4

--- findingaids/__init__.py
+++ findingaids/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = (1, 0, 4, 'pre')
+__version_info__ = (1, 0, 4, None)
 
 # Dot-connect all but the last. Last is dash-connected if not None.
 __version__ = '.'.join(str(i) for i in __version_info__[:-1])
