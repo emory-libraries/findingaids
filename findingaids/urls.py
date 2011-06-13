937e450b1810762e664792d604a8991b91b7f497
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-06-13 19:17:42 +0000

    add generic robots.txt to avoid getting 404 errors on robots.txt requests

--- findingaids/urls.py
+++ findingaids/urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-   (r'^db-admin/', include(admin.site.urls)),
+   url(r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+   url(r'^db-admin/', include(admin.site.urls)),
    url(r'^admin/', include('findingaids.fa_admin.urls', namespace='fa-admin')),
    url(r'^$', 'findingaids.content.views.site_index', name='site-index'),
    url(r'^content/', include('findingaids.content.urls', namespace='content')),
    # everything else should fall through to the main app
    url(r'^', include('findingaids.fa.urls', namespace='fa')),
 )
+
 
 # DISABLE THIS IN PRODUCTION
 if settings.DEV_ENV:
