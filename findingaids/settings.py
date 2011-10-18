af926e627d310753ddc0c42dde4696df4099e6ca
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-10-18 21:24:19 +0000

    revise middleware classes for django 1.3

--- findingaids/settings.py
+++ findingaids/settings.py
@@ -85,9 +85,9 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.http.ConditionalGetMiddleware',
 )
 
