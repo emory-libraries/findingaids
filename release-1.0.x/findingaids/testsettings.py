2b734af747cd79b666d7759064d1eb0d09db42a5
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-12-16 19:13:16 +0000

    rollback r1211 (tag typo)

--- release-1.0.x/findingaids/testsettings.py
+++
@@ -1,7 +0,0 @@
-from settings import *
-
-# this setting is needed for unit tests involving celery tasks
-# so the test doesn't hang
-# NOTE: this setting must be set before other things happen or it doesn't work
-CELERY_ALWAYS_EAGER = True
-
