13e2454b3374d2ef2fa2a51621aced6062bdd7ab
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2012-09-10 18:28:12 +0000

    bump version to 1.0.12-pre

--- findingaids/__init__.py
+++ findingaids/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = (1, 0, 11, 'pre')
+__version_info__ = (1, 0, 12, 'pre')
 
 # Dot-connect all but the last. Last is dash-connected if not None.
 __version__ = '.'.join(str(i) for i in __version_info__[:-1])
