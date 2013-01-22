7676514d33068b04de2955e2af61b3f9262b2617
rlskoeser <rebecca.s.koeser@emory.edu>
2013-01-22 11:28:43 -0500

    set default remote account in fabfile

--- fabfile.py
+++ fabfile.py
@@ -56,7 +56,7 @@ env.git_rev = ''
 env.remote_path = '/home/httpd/sites/findingaids'
 env.url_prefix = None
 env.remote_proxy = None
-
+env.remote_acct = 'findingaids'
 
 def configure(path=None, user=None, url_prefix=None, remote_proxy=None):
     'Configuration settings used internally for the build.'
