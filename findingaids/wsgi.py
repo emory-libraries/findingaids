9510b0e4435d21dc19f922d79880edd698118055
rlskoeser <rebecca.s.koeser@emory.edu>
2013-01-24 16:49:16 -0500

    update wsgi file to *only* set a localhost http proxy if one proxy is not set in env

--- findingaids/wsgi.py
+++ findingaids/wsgi.py
@@ -3,7 +3,12 @@ import djcelery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'findingaids.settings')
 os.environ['PYTHON_EGG_CACHE'] = '/tmp'
-os.environ['HTTP_PROXY'] = 'http://localhost:3128/'
+
+# Only set a default proxy if there is not already one in the env.
+# For production environments, proxy should be set in apache environment.
+if 'HTTP_PROXY' not in os.environ:
+    os.environ['HTTP_PROXY'] = 'http://localhost:3128/'
+
 os.environ['VIRTUAL_ENV'] = '/home/httpd/findingaids/env/'
 
 djcelery.setup_loader()
