170c6033757d2bcd84c7215a42ce6e75be50c098
rlskoeser <rebecca.s.koeser@emory.edu>
2013-03-05 12:31:38 -0500

    remove unused MEDIA_URL setting; update to latest version of eullocal (genlib template updated for staticfiles)

--- findingaids/settings.py
+++ findingaids/settings.py
@@ -70,7 +70,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'sitemedia')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media'
+# NOTE: user-uploaded media currently unused in this site
+# MEDIA_URL = '/media'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
