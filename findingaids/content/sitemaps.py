2ac227af470dd3f2b99ea3426dd37fc98d01f113
rlskoeser <rebecca.s.koeser@emory.edu>
2013-03-06 15:32:01 -0500

    clean up sitemaps for content pages [#45319013]

--- findingaids/content/sitemaps.py
+++ findingaids/content/sitemaps.py
@@ -33,23 +33,33 @@ class ContentPage(object):
 
 class ContentSitemap(Sitemap):
     changefreq = 'yearly'    # this content changes very rarely
+    default_priority = 0.4   # lower priority than actual findingaid content
 
     def items(self):
-        items = []
+        # special case pages
+        # non-findingaid site pages not based on feed content
+        items = [
+            ContentPage(reverse('site-index')),
+            ContentPage(reverse('content:request-materials')),
+            ContentPage(reverse('content:feedback')),
+            # possibly also advanced search? (not really content to index)
+        ]
 
         # collect all content pages via content RSS feed
+        # NOTE: feed entry includes publication date, but it is not
+        # included here because we can't rely on publication
+        # date being the same as last modification date
         for entry_id, entry in ContentFeed().get_items().iteritems():
             items.append(ContentPage(
                 url=reverse('content:page', kwargs={'page': entry_id})
             ))
-            # NOTE: feed entry includes publication date, but not
-            # including here because we can't rely on publication
-            # date being the same as last modification date
-
-        # two special cases (non-feed content pages)
-        items.extend([
-            ContentPage(reverse('content:request-materials')),
-            ContentPage(reverse('content:feedback')),
-        ])
 
         return items
+
+    def priority(self, item):
+        # set home page to higher priority
+        # NOTE: faq and search tips might also possibly be higher
+        # priority than other pages
+        if item.get_absolute_url() == reverse('site-index'):
+            return 0.5
+        return self.default_priority
