4a4cf6abc9b63b20238aca24089e63f404d470b2
rlskoeser <rebecca.s.koeser@emory.edu>
2013-10-17 15:47:21 -0400

    configure django admin site for archives & associating users with them [#58160956] (missed checkin)

---
+++ findingaids/fa/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from findingaids.fa.models import Deleted, Archive
+
+class DeletedAdmin(admin.ModelAdmin):
+    list_display = ('eadid', 'title', 'date', 'note')
+    list_filter = ('date',)
+
+    # don't allow creating deleted records via admin site
+    def has_add_permission(self, request):
+        return False
+
+admin.site.register(Deleted, DeletedAdmin)
+
+
+class ArchiveAdmin(admin.ModelAdmin):
+    list_display = ('label', 'name', 'svn')
+
+admin.site.register(Archive, ArchiveAdmin)
---
+++ findingaids/fa_admin/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+# from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.models import User
+from eullocal.django.emory_ldap.admin import EmoryLDAPUserAdmin
+
+from findingaids.fa_admin.models import Archivist
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class ArchivistInline(admin.StackedInline):
+    model = Archivist
+    can_delete = False
+    verbose_name_plural = 'archivist'
+
+# Define a new User admin
+class UserAdmin(EmoryLDAPUserAdmin):
+    inlines = (ArchivistInline, )
+    list_filter = ('archivist__archives', 'is_staff', 'is_superuser',
+                   'is_active', 'groups')
+
+
+# Re-register UserAdmin
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), UserAdmin)
