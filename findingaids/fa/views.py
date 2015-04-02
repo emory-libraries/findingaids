01f46f72c1e685e239e86979bdf69b41f7e61031
rlskoeser <rebecca.s.koeser@emory.edu>
2015-04-02 10:49:09 -0400

    include ead repository on series page to allow determining of ead is requestable [#91633476]

--- findingaids/fa/views.py
+++ findingaids/fa/views.py
@@ -411,6 +411,7 @@ def _get_series_or_index(eadid, *series_ids, **kwargs):
                      'ead__archdesc__origination',
                      'ead__archdesc__controlaccess__head', 'ead__dsc__head',
                      'ead__origination_name',
+                     'ead__repository',  # needed to determine if requestable
                      'ead__collection_id']
     # common search parameters - last series id should be requested series, of whatever type
     search_fields = {'ead__eadid': eadid, 'id': series_ids[-1]}
