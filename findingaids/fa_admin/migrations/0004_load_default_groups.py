20126fd25cbd29e74ea874bad5b70d6ac4a282c9
rlskoeser <rebecca.s.koeser@emory.edu>
2016-04-07 11:01:13 -0400

    Update group fixture migration so permissions are available
    [#95254830]

--- findingaids/fa_admin/migrations/0004_load_default_groups.py
+++ findingaids/fa_admin/migrations/0004_load_default_groups.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.core.management import call_command
+from django.contrib.auth.management import create_permissions
 
 
 DEFAULT_GROUPS = ["Editors", "Publishers"]
@@ -10,8 +11,15 @@ DEFAULT_GROUPS = ["Editors", "Publishers"]
 def load_fixture(apps, schema_editor):
     'Load default groups.'
     # load initial groups if they are not already present
+
+    # ensure permissions are flushed before loading groups that require them
+    # per http://stackoverflow.com/questions/29296757/django-data-migrate-permissions
+    apps.models_module = True
+    create_permissions(apps, verbosity=0)
+    apps.models_module = None
+
     Group = apps.get_model("auth", "Group")
-    if Group.objects.filter(name__in=DEFAULT_GROUPS).count() == 0:
+    if Group.objects.filter(name__in=DEFAULT_GROUPS).count() != len(DEFAULT_GROUPS):
         call_command('loaddata', 'initial_groups', app_label='fa_admin')
 
 def unload_fixture(apps, schema_editor):
@@ -27,7 +35,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('fa_admin', '0003_emoryldap_user_to_auth_user'),
         ('auth', '0001_initial'),
-        ('contenttypes', '__first__'),
+        ('contenttypes', '0001_initial'),
         # requires permissions references for other models:
         ('fa', '0001_initial'),
     ]
